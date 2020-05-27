32514a1474b8be3edfb3eb5a3488b7d47748f505
Miles Cooley <treefrogman@gmail.com>
2020-05-27 15:35:30 -0700

    Abstracted more syles out into CSS
    - Gradient stop colors are now based on global color variables
    - Stroke thickness is now based on global variables

--- js/svgDefs.js
+++ js/svgDefs.js
@@ -3,12 +3,12 @@ export default {
 		let radialGradient = svg.createElement("radialGradient");
 		radialGradient.id = "s0cketGradient";
 		let start = svg.createElement("stop");
+		start.id = "s0cketGradientStart";
 		start.setAttribute("offset", "10%");
-		start.setAttribute("stop-color", "white");
 		radialGradient.appendChild(start);
 		let end = svg.createElement("stop");
+		end.id = "s0cketGradientEnd";
 		end.setAttribute("offset", "15%");
-		end.setAttribute("stop-color", "rgba(255, 255, 255, 0)");
 		radialGradient.appendChild(end);
 		return radialGradient;
 	}
