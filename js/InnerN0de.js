dbf3f85463169270c6cb32187bf0a1252cd0e179
Miles Cooley <treefrogman@gmail.com>
2020-06-18 02:35:04 -0700

    Commented S0cket and InnerN0de
    Added comments and JSDoc
    Many intended changes (refactoring, etc) were noted but not implemented
    I didn't want to muddle the commenting session with refactoring

--- js/InnerN0de.js
+++ js/InnerN0de.js
@@ -1,33 +1,77 @@
 import AbstractN0de from './AbstractN0de.js'
 import margins from './margins.js'
 
+/** The InnerN0de class is used by the {@link N0deView} class to display all the nødes within the current view.
+ * @extends AbstractN0de
+ */
 class InnerN0de extends AbstractN0de {
+
+	/**
+	 * @todo Remove the n0deView parameter. It isn't used anywhere.
+	 * @param {SVG} svgArg - SVG object shared among all components. See {@link SVG} for details.
+	 * @param {N0deView} n0deView - Unused parameter: the parent view.
+	 * @param {string} id - UUID of the nøde.
+	 * @param {string} type - Human-readable name of the nøde.
+	 * @param {array} position - X and Y coordinates of the nøde, as an array.
+	 * @param {object} s0ckets - JSON object containing arrays of input and output søckets.
+	 */
 	constructor(svgArg, n0deView, id, type, position, s0ckets) {
+
+		// Run the AbstractN0de constructor.
 		super(svgArg, n0deView, id, type, s0ckets);
 		
+		// Add the frame element to the root element of the nøde as the first child so that it's behind all the søckets and the title.
 		this.element.prepend(this.frame);
 		this.frame.classList.add("innerN0deFrame");
+		// Offset the frame. See margins.js for complete explanation of offset.
 		this.frame.setAttribute("x", margins.offset);
 		this.frame.setAttribute("y", margins.offset);
 
+		// Set the root element position based on position parameter and offset. See margins.js for complete explanation of offset.
 		this.position = [position[0] - margins.offset, position[1] - margins.offset];
 		this.element.setAttribute("x", this.position[0]);
 		this.element.setAttribute("y", this.position[1]);
 		this.element.classList.add("innerN0de");
+
+		// Size the nøde to fit its components.
 		this.shrinkWrap();
 	}
+
+	/** Add an input or output søcket to the nøde.
+	 * @param {object} s0cketSpec - Object with three properties: <code>label</code>, <code>type</code>, and <code>id</code>. See {@link N0deApp} for details.
+	 * @param {string} inOut - Either <code>"in"</code> or <code>"out"</code>. Specifies whether the søcket is an input or an output.
+	 * @param {integer} index - Where in the list of input or output søckets this søcket resides.
+	 * @memberof InnerN0de
+	 */
 	addS0cket(s0cketSpec, inOut, index) {
 		super.addS0cket(s0cketSpec, "inner", inOut, index);
 	}
+	
+	/** Automatically resize the nøde frame to just fit the title and søckets.
+	 * @memberof InnerN0de
+	 */
 	shrinkWrap() {
+
+		// CALCULATE NEW WIDTH
+		// Iterate over ALL the søckets (both input and output), and for each row add up the width of both søckets.
+		// 		This does funky stuff. Could simplify loop if array is initialized with Array(maxHeight).fill(0)...
 		let s0cketWidths = [];
 		[...this.s0ckets.in, ...this.s0ckets.out].forEach(function (s0cket) {
 			s0cketWidths[s0cket.index] = (s0cketWidths[s0cket.index] || 0) + s0cket.getLabelWidth();
 		});
+
+		// Find the widest out of all the rows and the title.
 		let maxWidth = Math.max(...s0cketWidths, this.titleObject.bbox.width);
-		let maxHeight = Math.max(this.s0ckets.in.length, this.s0ckets.out.length);
+		// Round up to an integer and add space.
 		let newWidth = Math.ceil(maxWidth) + margins.s0ckets.horizontalSpacing;
+
+		// CALCULATE NEW HEIGHT
+		// How many rows of søckets are there?
+		let maxHeight = Math.max(this.s0ckets.in.length, this.s0ckets.out.length);
+		// Multiply rows by preset spacing and round up to an integer.
 		let newHeight = Math.ceil(maxHeight * margins.s0ckets.verticalSpacing);
+
+		// Apply the new height and width.
 		this.resize([newWidth, newHeight]);
 	}
 }
--- js/S0cket.js
+++ js/S0cket.js
@@ -1,47 +1,78 @@
+/** @module S0cket */
+
 import margins from './margins.js'
 
+// TODO: remove
 let svg = null;
 
-/*
-The søcket class:
-- Creates all the graphical elements associated with the søcket and reports their bounding boxes
-- Updates those graphical elements in response to commands from the controller
-- Notifies the controller of interactions with the søcket and its labels
-*/
+/**The S0cket class creates and manages all the graphical elements associated with a søcket */
 class S0cket {
+
+	/**
+	 * @param {SVG} svgArg - 
+	 * @param {AbstractN0de} n0de - Unused parameter: the parent nøde.
+	 * @param {string} innerOuter - Either <code>"inner"</code> or <code>"outer"</code>. Specifies whether the søcket belongs to an InnerN0de or an OuterN0de.
+	 * @todo Possible to refactor so innerOuter is not required? It just seems clunky...
+	 * @param {string} inOut - Either <code>"in"</code> or <code>"out"</code>. Specifies whether the søcket is an input or an output.
+	 * @param {integer} index - Unused parameter: Where in the list of input or output søckets this søcket resides.
+	 * @param {string} label - Human readable name of the søcket's role in the context of the parent nøde.
+	 * @param {string} type - Human readable name of the søcket's type.
+	 * @param {string} id - Unused parameter: UUID of the søcket's type.
+	 */
 	constructor(svgArg, n0de, innerOuter, inOut, index, label, type, id) {
 		svg = svgArg;
 
-		this.n0de = n0de;
-		this.inOut = inOut;
-		this.index = index;
-		this.id = id;
+		this.n0de = n0de; // Unused property
+		this.inOut = inOut; // Unused property
+		this.index = index; // Unused property
+		this.id = id; // Unused property
 
+		// SVG element to contain all the visual components of the søcket
 		this.element = svg.createElement("svg");
 		this.position = [0, 0];
 
+		// The graphical core of a søcket
 		this.circle = drawCircle();
 		this.element.appendChild(this.circle);
 
+		// LABEL
 		this.label = label;
 		this.labelText = drawLabel(label, inOut, innerOuter);
 		this.element.appendChild(this.labelText);
 
+		// TYPE
 		this.type = type;
 		this.typeText = drawType(type, inOut, innerOuter);
 		this.element.appendChild(this.typeText);
 
+		// CLICK HITBOX
 		this.clickZone = drawClickZone();
 		this.element.appendChild(this.clickZone);
 	}
+
+	/** Return the root SVG element of the S0cket object.
+	 * @deprecated Will likely switch to referencing the element property directly.
+	 * @returns {SVGSVGElement} The root SVG element.
+	 * @memberof S0cket
+	 */
 	getElement() {
 		return this.element;
 	}
+
+	// 
+	/** Set the position of the søcket.
+	 * @param {array} position - X and Y coordinates of the søcket, as an array.
+	 * @memberof S0cket
+	 */
 	setPosition(position) {
 		this.position = position;
 		this.element.setAttribute("x", position[0] - margins.offset);
 		this.element.setAttribute("y", position[1] - margins.offset);
 	}
+	/** Return the width of the søcket label.
+	 * @returns {number} The width of the søcket label.
+	 * @memberof S0cket
+	 */
 	getLabelWidth() {
 		return svg.getBBox(this.labelText).width;
 	}
@@ -61,6 +92,11 @@ const leftRight = [
 ];
 
 // Choose correct rendering parameters given information about the context
+// @param {string} labelType - Either "label" or "type", specifying which kind of søcket text we're working with.
+// @param {string} inOut - Either "in" or "out", specifying whether this is an input or output søcket.
+// @param {string} innerOuter - Either "inner" or "outer", specifying whether this søcket belongs to an InnerN0de or an OuterN0de.
+// @returns {object} - 
+
 function s0cketTextSide(labelType, inOut, innerOuter) {
 
 	// Use bitwise XOR to flip-flop on each of three booleans:
@@ -71,8 +107,11 @@ function s0cketTextSide(labelType, inOut, innerOuter) {
 	return leftRight[(labelType == "label") ^ (inOut == "in") ^ (innerOuter == "inner")];
 }
 
+// Test s0cketTextSide
 console.log(s0cketTextSide("label", "out", "outer"));
 
+// Draw the circle element
+// TODO: This doesn't really need to be in a function, so its content could be moved to the constructor
 function drawCircle() {
 	let s0cketCircle = svg.createElement("circle");
 	s0cketCircle.setAttribute("cx", margins.offset);
@@ -81,9 +120,11 @@ function drawCircle() {
 	return s0cketCircle;
 }
 
+// Draw the label text
 function drawLabel(label, inOut, innerOuter) {
 	let labelText = svg.createElement("text");
 	labelText.textContent = label;
+	// TODO: refactor to use s0cketTextSide only once and store value.
 	labelText.setAttribute("text-anchor", s0cketTextSide("label", inOut, innerOuter).anchor);
 	labelText.setAttribute("x", margins.offset + s0cketTextSide("label", inOut, innerOuter).offset);
 	labelText.setAttribute("y", margins.offset);
@@ -91,9 +132,12 @@ function drawLabel(label, inOut, innerOuter) {
 	return labelText;
 }
 
+// Draw the type text
+// TODO: This is identical to drawLabel, save for select few differences which could be toggled by a parameter
 function drawType(type, inOut, innerOuter) {
 	let typeText = svg.createElement("text");
 	typeText.textContent = type;
+	// TODO: refactor to use s0cketTextSide only once and store value.
 	typeText.setAttribute("text-anchor", s0cketTextSide("type", inOut, innerOuter).anchor);
 	typeText.setAttribute("x", margins.offset + s0cketTextSide("type", inOut, innerOuter).offset);
 	typeText.setAttribute("y", margins.offset);
@@ -101,6 +145,7 @@ function drawType(type, inOut, innerOuter) {
 	return typeText;
 }
 
+// Draw the invisible box that receives mouse events
 function drawClickZone() {
 	let clickZone = svg.createElement("rect");
 	let widthHeight = margins.s0ckets.verticalSpacing;
@@ -113,6 +158,7 @@ function drawClickZone() {
 	return clickZone;
 }
 
+// Unused function: delete??? 🤔🤔
 function drawS0cketLabel() { }
 
 export default S0cket
