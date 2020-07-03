392991b37e28b73e8f06bc43e12b87b4bcafebb0
Miles Cooley <treefrogman@gmail.com>
2020-07-03 11:37:51 -0700

    SVG: add JSDoc comments, remove xlink namespace
    The xlink namespace is deprecated from SVG href attributes.
    https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/xlink:href

--- js/SVG.js
+++ js/SVG.js
@@ -1,8 +1,12 @@
 // SVG namespace declarations
 const svgNS = "http://www.w3.org/2000/svg";
-const xlinkNS = "http://www.w3.org/1999/xlink";
 
+
+/** The SVG object handles certain management tasks for the main SVG element, and provides helper functions for SVG elements. */
 class SVG {
+	/**
+	 * @param {HTMLDocument} document - The HTML document that will contain all the graphical elements.
+	 */
 	constructor(document) {
 
 		// Create the main SVG element. This will be the root element for all view components.
@@ -16,24 +20,36 @@ class SVG {
 		this.mainSVG.appendChild(this.defsElement);
 	}
 
-	// Add the provided element to the defs element.
-	// Elements in defs are not rendered, but may be referenced by other elements.
+	/** Add the provided element to the defs element. Note: Elements in defs are not rendered, but may be referenced by other elements.
+	 * @param {SVGElement} def - The element to add to the defs element.
+	 * @memberof SVG
+	 */
 	addDef(def) {
 		this.defsElement.appendChild(def);
 	}
 
-	// Return the main SVG element.
+	/** Return the main SVG element.
+	 * @returns {SVGSVGElement} The root SVG element.
+	 * @memberof SVG
+	 */
 	getElement() {
 		return this.mainSVG;
 	}
 
-	// Create a new SVG element. This is a wrapper for document.createElementNS.
+	/** Create a new SVG element. This is a wrapper for document.createElementNS().
+	 * @param {string} tagName - The type of SVG element to create.
+	 * @returns {SVGElement} The newly created SVG element.
+	 * @memberof SVG
+	 */
 	createElement(tagName) {
 		return document.createElementNS(svgNS, tagName);
 	}
 
-	// Calculate the bounding box of the provided element.
-	// This requires adding it to an SVG element that is in the DOM.
+	/** Calculate the bounding box of the provided element. To do this, getBBox adds a clone of the element to a temporary SVG root element in the DOM and then queries the element for its bounding box.
+	 * @param {SVGElement} element - The element whose bounding box should be calculated.
+	 * @returns {SVGRect} An object containing x, y, width, and height properties.
+	 * @memberof SVG
+	 */
 	getBBox(element) {
 
 		// Make a clone of the element so that when it is added to the prerenderingSVG
