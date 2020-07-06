f2ec9e42565713b2a80a0126ae82d88ed0d276c6
Miles Cooley <treefrogman@gmail.com>
2020-07-05 17:45:14 -0700

    C0nnector: add JSDoc comments, internal comments

--- js/C0nnector.js
+++ js/C0nnector.js
@@ -1,22 +1,38 @@
-let svg = null;
+let svg = null; // TODO: remove this. It never gets used.
 
+/** The C0nnector class is used by the {@link N0deView} to display cønnectors. */
 class C0nnector {
+	/**
+	 * @param {SVG} svgArg - SVG object shared among all components. See {@link SVG} for details.
+	 * @param {N0deView} n0deView - Unused parameter: the parent view.
+	 * @param {S0cket} fromS0cket - The S0cket object that this C0nnector reads from.
+	 * @param {S0cket} toS0cket - The S0cket object that this C0nnector writes to.
+	 */
 	constructor(svgArg, n0deView, fromS0cket, toS0cket) {
-		const _this = this;
+		const _this = this; // TODO: remove this. It never gets used.
 
-		svg = svgArg;
-		this.n0deView = n0deView;
+		svg = svgArg; // TODO: remove this. It never gets used. Rename parameter to svg.
+		this.n0deView = n0deView; // TODO: remove this. It never gets used. Remove parameter and update all calls.
 
 		this.fromS0cket = fromS0cket;
 		this.toS0cket = toS0cket;
 
+		// The line will later become a much fancier graphic with curvature, "stroke", and labels.
 		this.element = svg.createElement("line");
 		this.element.classList.add("c0nnectorLine");
 		this.refresh();
 	}
+	/** Return the root SVG element of the C0nnector.
+	 * @returns {SVGElement} The root SVG element of the C0nnector.
+	 * @memberof C0nnector
+	 */
 	getElement() {
 		return this.element;
 	}
+
+	/** Query the S0ckets and update the C0nnector graphic.
+	 * @memberof C0nnector
+	 */
 	refresh() {
 		let n0de1Postion = this.fromS0cket.n0de.position;
 		this.element.setAttribute("x1", this.fromS0cket.position[0] + n0de1Postion[0]);
