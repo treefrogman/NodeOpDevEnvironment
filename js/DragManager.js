e81eaa8cc653f6c3ed727b6198b8008faf549ad9
Miles Cooley <treefrogman@gmail.com>
2020-11-17 20:35:57 -0800

    Implement DragManager and DragEpisode classes

---
+++ js/DragManager.js
@@ -0,0 +1,65 @@
+class DragManager {
+	constructor() {
+		this.dragEpisodes = [];
+		const thisDragManager = this;
+		window.addEventListener("mousemove", (event) => {
+			thisDragManager.dragEpisodes.forEach((episode) => {
+				if (episode.active) {
+					episode.triggerMoveEvent(event);
+				}
+			});
+		});
+		window.addEventListener("mouseup", (event) => {
+			thisDragManager.dragEpisodes.forEach((episode) => {
+				if (episode.active) {
+					episode.triggerDropEvent(event);
+				}
+			});
+		});
+	}
+	initiateDragEpisode(event) {
+		const newDragEpisode = new DragEpisode(event);
+		this.dragEpisodes.push(newDragEpisode);
+		return newDragEpisode;
+	}
+
+}
+
+class DragEpisode {
+	constructor(event) {
+		this.initiatingEvent = event;
+		this.moveCallbacks = [];
+		this.dropCallbacks = [];
+		this.active = true;
+		console.log(this);
+	}
+
+	addMoveCallback(callback) {
+		this.moveCallbacks.push(callback);
+		return this;
+	}
+
+	triggerMoveEvent(event) {
+		this.delta = [event.clientX - this.initiatingEvent.clientX, event.clientY - this.initiatingEvent.clientY];
+		const thisDragEpisode = this;
+		this.moveCallbacks.forEach((callback) => {
+			callback(thisDragEpisode);
+		});
+	}
+
+	addDropCallback(callback) {
+		this.dropCallbacks.push(callback);
+		return this;
+	}
+
+	triggerDropEvent(event) {
+		this.delta = [event.clientX - this.initiatingEvent.clientX, event.clientY - this.initiatingEvent.clientY];
+		this.active = false;
+		const thisDragEpisode = this;
+		this.dropCallbacks.forEach((callback) => {
+			callback(thisDragEpisode);
+		});
+	}
+}
+
+export default DragManager
