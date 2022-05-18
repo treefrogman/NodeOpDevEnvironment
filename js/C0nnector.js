cdd8f27fb2975c6eb1a261e1a4642239318f1727
Miles Cooley <treefrogman@gmail.com>
2022-05-17 20:39:28 -0700

    Cønnector curves

--- js/C0nnector.js
+++ js/C0nnector.js
@@ -1,3 +1,65 @@
+class Vector2D {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+	add(v) {
+		return new Vector2D(this.x + v.x, this.y + v.y);
+	}
+	sub(v) {
+		return new Vector2D(this.x - v.x, this.y - v.y);
+	}
+	mul(s) {
+		return new Vector2D(this.x * s, this.y * s);
+	}
+	length() {
+		return Math.sqrt(this.x * this.x + this.y * this.y);
+	}
+	angle() {
+		return Math.atan2(this.y, this.x);
+	}
+	normalize() {
+		let l = this.length();
+		return new Vector2D(this.x / l, this.y / l);
+	}
+	reach(l) {
+		return this.normalize().mul(l);
+	}
+	rotate(rad) {
+		return new Vector2D(
+			this.x * Math.cos(rad) - this.y * Math.sin(rad),
+			this.x * Math.sin(rad) + this.y * Math.cos(rad)
+		);
+	}
+	rotateAround(rad, v) {
+		return this.sub(v).rotate(rad).add(v);
+	}
+	toString() {
+		return `${this.x}, ${this.y}`;
+	}
+}
+function v(x, y) {
+	if (x instanceof Vector2D) return x;
+	if (x.x !== undefined) return new Vector2D(x.x, x.y);
+	if (Array.isArray(x)) return new Vector2D(x[0], x[1]);
+	return new Vector2D(x, y);
+}
+function connectorCurve(p1, p2) {
+	[p1, p2] = [v(p1), v(p2)];
+	const diff = p2.sub(p1);
+	const rad = Math.min(diff.length() * .2, 50);
+	const tanDiff = diff.add(v(0, rad * 2 * (diff.y > 0 ? -1 : 1)))
+	const tanDist = tanDiff.length();
+	const tanTerm = tanDist**2 - (rad*2)**2;
+	const tanRad = tanTerm **.5;
+	const numerator = tanTerm + tanRad**2;
+	const x = numerator/(2*tanDist);
+	const y = ((4*tanDist**2*tanRad**2 - numerator**2)/(4*tanDist**2))**.5 * (diff.y <= 0 ? -1 : 1);
+	const angle = tanDiff.reach(x).add(tanDiff.rotate(Math.PI/2).reach(y)).angle();
+	const arcEnd = v(0, 0).rotateAround(angle, v(0, (diff.y <= 0 ? -1 : 1) * rad));
+	return `M${p1} A${rad},${rad} 0 ${angle * diff.y <= 0 ? 1 : 0},${diff.y <= 0 ? 0 : 1} ${p1.add(arcEnd)} L${p2.sub(arcEnd)} A${rad},${rad} 0 ${angle * diff.y <= 0 ? 1 : 0},${diff.y <= 0 ? 1 : 0} ${p2}`;
+}
+
 /** The C0nnector class is used by the {@link N0deView} to display cønnectors. */
 class C0nnector {
 	/**
@@ -10,8 +72,8 @@ class C0nnector {
 		this.fromS0cket = fromS0cket;
 		this.toS0cket = toS0cket;
 
-		// The line will later become a much fancier graphic with curvature, "stroke", and labels.
-		this.element = svg.createElement("line");
+		// The curve will later get a "stroke" to set it off any background, labels, and animated døts moving along it.
+		this.element = svg.createElement("path");
 		this.element.classList.add("c0nnectorLine");
 		this.refresh();
 	}
@@ -20,12 +82,10 @@ class C0nnector {
 	 * @memberof C0nnector
 	 */
 	refresh() {
-		let n0de1Postion = this.fromS0cket.n0de.position;
-		this.element.setAttribute("x1", this.fromS0cket.position[0] + n0de1Postion[0]);
-		this.element.setAttribute("y1", this.fromS0cket.position[1] + n0de1Postion[1]);
-		let n0de2Position = this.toS0cket.n0de.position;
-		this.element.setAttribute("x2", this.toS0cket.position[0] + n0de2Position[0]);
-		this.element.setAttribute("y2", this.toS0cket.position[1] + n0de2Position[1]);
+		let p1 = v(this.fromS0cket.n0de.position).add(v(this.fromS0cket.position))
+		let p2 = v(this.toS0cket.n0de.position).add(v(this.toS0cket.position))
+		let d = connectorCurve(p1, p2);
+		this.element.setAttribute("d", d);
 	}
 }
 
